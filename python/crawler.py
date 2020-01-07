69e5994973d84d220a93f9560fd00a28e6ef13c8
yiji-gr <516123131@qq.com>
2020-01-07 16:43:00 +0800

    crawler

--- python/crawler.py
+++ python/crawler.py
@@ -12,19 +12,19 @@ urls = re.findall('<td><a href="(.*?)">.*?</a></td>', page1.text)
 idx = 0
 line_limit = 50
 with open("mcnxs.txt", 'w') as f:
-	for url2 in urls:
-		idx += 1
-		print(url2, "第"+str(idx)+"章")
-		page2 = requests.get(url2, headers=headers)
-		html = etree.HTML(page2.content)
-		title = html.xpath('/html/body/div[3]/h1')[0].text
-		contents = html.xpath('/html/body/div[3]/div[2]/p')
-		print('  ' * ((line_limit - len(title)) // 2) + title, file=f)
+    for url2 in urls:
+	idx += 1
+	print(url2, "第"+str(idx)+"章")
+	page2 = requests.get(url2, headers=headers)
+	html = etree.HTML(page2.content)
+	title = html.xpath('/html/body/div[3]/h1')[0].text
+	contents = html.xpath('/html/body/div[3]/div[2]/p')
+	print('  ' * ((line_limit - len(title)) // 2) + title, file=f)
 
-		for i in range(len(contents) - 1):
-			line = contents[i].text
-			length = len(line)
-			for j in range(length // line_limit):
-				print(line[(j*line_limit):(line_limit+j*line_limit)], file=f)
-			print(line[length // line_limit * line_limit:] + '\n', file=f)
+	for i in range(len(contents) - 1):
+   	    line = contents[i].text
+	    length = len(line)
+	    for j in range(length // line_limit):
+		print(line[(j*line_limit):(line_limit+j*line_limit)], file=f)
+	    print(line[length // line_limit * line_limit:] + '\n', file=f)
 
